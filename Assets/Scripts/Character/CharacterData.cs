9470fbdc58e74bc3634734adb61ac8c1da39d061
Josh Thompson <jt244345@falmouth.ac.uk>
2022-08-24 14:32:52 +0100

    Added character-stats to the repo

---
+++ Assets/Scripts/Character/CharacterData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+namespace JoshGames.Stats{
+    public class CharacterData : MonoBehaviour{
+        #region ASSIGNED STATS
+        [SerializeField] CharacterStats profile;
+        public CharacterStats Profile{ get{ return profile; } }
+
+        [Header("Realtime stats -- You don't have to touch these")]
+        public float health;
+        public float speed;
+        public float damage;
+        public int threatLevel;
+        public CharacterStats.Trait behaviour;
+        public List<CharacterStats.Buff> buffs = new List<CharacterStats.Buff>();
+        public List<CharacterStats.Debuff> debuffs = new List<CharacterStats.Debuff>();
+        #endregion
+        private void Start() {
+            if(!profile){ return; }
+
+            health = profile.health;
+            speed = profile.speed;
+            damage = profile.damage;
+            threatLevel = profile.threatLevel;
+            behaviour = profile.behaviour;
+            buffs = profile.buffs.ToList();            
+        }
+    }
+}

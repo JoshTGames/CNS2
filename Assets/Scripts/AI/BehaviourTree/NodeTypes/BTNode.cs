8b99b69584894b134391ce21399021851ff25ad7
Josh Thompson <jt244345@falmouth.ac.uk>
2022-08-24 14:30:34 +0100

    Added behaviours to the repo

---
+++ Assets/Scripts/AI/BehaviourTree/NodeTypes/BTNode.cs
@@ -0,0 +1,14 @@
+﻿namespace JoshGames.AI{
+    public abstract class BTNode{
+        protected NodeState state;
+        public NodeState State{ get { return state; } }
+        public abstract NodeState Evaluate();
+    }
+    
+
+    public enum NodeState{
+        FAILURE,
+        SUCCESS,
+        RUNNING
+    }
+}
---
+++ Assets/Scripts/AI/BehaviourTree/NodeTypes/BTSequence.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace JoshGames.AI{
+    public class BTSequence : BTNode{
+        protected List<BTNode> nodes = new List<BTNode>();
+
+        public BTSequence(List<BTNode> _nodes){ this.nodes = _nodes; }
+        public override NodeState Evaluate(){
+            bool isAnyRunning = false;
+            foreach(BTNode node in nodes){
+                switch(node.Evaluate()){
+                    case NodeState.RUNNING:
+                        isAnyRunning = true;
+                        break;
+                    case NodeState.SUCCESS:
+                        break;
+                    case NodeState.FAILURE:
+                        return NodeState.FAILURE;
+                }
+            }
+            return (isAnyRunning)? NodeState.RUNNING : NodeState.SUCCESS;
+        }
+    }
+}

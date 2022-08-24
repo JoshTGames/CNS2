df948bd858e2258fbd5b5da8620b5746ed25a406
Josh Thompson <jt244345@falmouth.ac.uk>
2022-08-24 14:26:20 +0100

    Added BT templates to the repo

---
+++ Assets/Scripts/AI/BehaviourTemplate/AIModule.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JoshGames.Stats;
+
+namespace JoshGames.AI{
+    public class AIModule : MonoBehaviour{
+
+        static BTNode topNode;
+        public static BTNode AI{ 
+            get{ return (topNode != null)? topNode : CreateBehaviourTree(); } 
+        }
+
+
+        static BTNode CreateBehaviourTree(){
+            #region NODES HERE
+            #endregion
+
+            // BEHAVIOURS --
+            // --
+
+
+
+            BTSelector parentNode = new BTSelector(new List<BTNode>{/* PUT NODES HERE */}); // LAST NODE SHOULD BE A FALLBACK NODE WHICH IS REPEATABLE AKA NEVER RETURN FAILURE
+
+            topNode = (topNode != null)? topNode : parentNode; 
+            return parentNode;
+        }
+
+
+        private void Awake() {
+            topNode = CreateBehaviourTree();    
+        }
+    }
+}
---
+++ Assets/Scripts/Character/CharacterStats.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+
+namespace JoshGames.Stats{
+    [CreateAssetMenu(fileName = "CharacterProfile", menuName = "ScriptableObjects/Character/Create CharacterProfile")]
+    public class CharacterStats : ScriptableObject{
+        [Header("Character BASE Stats")]
+        public float health;
+        public float speed;
+        public float damage;
+        
+        [Header("Extras")]
+        [Tooltip("This will dictate what type of threat a character is to other characters. If high enough, this will scare other characters")] public int threatLevel;
+        public Trait behaviour;
+        public Buff[] buffs;
+
+        [Serializable] public enum Trait{
+                                                                                                                                NONE,
+            [Tooltip("ALWAYS FLEE FROM TARGETS -- OF EQUAL OR HIGHER THREAT LEVEL")]                                             PASSIVE_FLEE, 
+            [Tooltip("ONLY FLEE WHEN HARMED")]                                                                                   PASSIVE, 
+            [Tooltip("FLEE FROM TARGETS OF HIGHER THREAT LEVEL / ATTACK TARGET OF EQUAL OR LESS THREAT LEVEL || - WHEN HARMED")] NEUTRAL,
+            [Tooltip("ALWAYS ATTACK TARGETS OF LOWER OR EQUAL THREAT LEVEL")]                                                    HOSTILE,        
+        }
+        [Serializable] public enum Buff{
+            NONE,
+            INF_AMMO,
+            DBL_DMG,
+            FIRE_PROOF,
+            STUN_PROOF,
+            PHYSICAL_RESISTANCE,
+            RANGE_RESISTANCE
+        }  
+
+        [Serializable] public enum Debuff{
+            NONE,            
+        }    
+    }
+}
